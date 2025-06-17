b74fd087bc0d4fe986bf2e71172f2bdfd1c9b5ca
Black Baron <black_baron94@hotmail.com>
2025-06-18 00:20:34 +0300

    deleted duplicates

--- frontend/script.js
+++ frontend/script.js
@@ -1,17 +1,19 @@
-async function translate() {
-  const input = document.getElementById("input").value;
-  const direction = document.getElementById("direction").value;
-  const resultEl = document.getElementById("result");
-
-  try {
-    const res = await fetch("https://latin-numerals-api.onrender.com/translate", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ input, direction }),
-    });
-    const data = await res.json();
-    resultEl.textContent = data.result;
-  } catch (e) {
-    resultEl.textContent = "Error: " + e.message;
-  }
+async function translate_clicked() {
+	console.log("potato");
+	const input = document.getElementById("input").value;
+	const direction = document.getElementById("direction").value;
+	const resultEl = document.getElementById("result");
+	
+	try{
+		const res = await fetch("https://latin-numerals-api.onrender.com/translate", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({input, direction}),
+		});
+		const data  = await res.json();
+		resultEl.textContent = data.result;
+	} catch (e) {
+		resultEl.textContent = "Error: " + e.message;
+	}
+	
 }
