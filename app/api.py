00f44b301948a16a33e90ef9d797f758294dc5c3
Black Baron <black_baron94@hotmail.com>
2025-06-18 03:13:37 +0300

    Made a simulacrum of a site resembling Latin Numerals APP

--- app/api.py
+++ app/api.py
@@ -1,18 +1,35 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from app.logic import handle_translation_input
+from app.logic import handle_translation_input, randomize, handle_quiz_input
 
 router = APIRouter()
 
 class TranslationRequest(BaseModel):
-    input: str
+    user_input : str
     direction: str  # "Latin to Decimal" or "Decimal to Latin"
+
+class CheckQuizAnswerRequest(BaseModel):
+    question_number: str
+    user_input : str
+    quiz_streak: int
 
 @router.get("/")
 def root():
     return {"message": "Welcome to Latin Numerals API"}
 
+
 @router.post("/translate")
 def translate(req: TranslationRequest):
-    result = handle_translation_input(req.input, req.direction)
+    result = handle_translation_input(req.user_input , req.direction)
     return {"result": result}
+
+
+@router.post("/quiz_query")
+def get_number():
+    number = randomize()
+    return {"number": number}
+    
+@router.post("/quiz_answer")
+def check_quiz_answer(req: CheckQuizAnswerRequest):
+    message, quiz_streak = handle_quiz_input(req.question_number, req.user_input , req.quiz_streak)
+    return {"result": message, "streak": quiz_streak}
--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,10 @@
+let streak = Number(sessionStorage.getItem("streak")) || 0;
+
 async function translate_clicked() {
-	console.log("potato");
 	const input = document.getElementById("input").value;
 	const direction = document.getElementById("direction").value;
 	const resultEl = document.getElementById("result");
+	resultEl.textContent = "Now loading result...";
 	
 	try{
 		const res = await fetch("https://latin-numerals-api.onrender.com/translate", {
@@ -17,3 +19,50 @@ async function translate_clicked() {
 	}
 	
 }
+
+async function submit_answer_clicked() {
+	const question_number = document.getElementById("question_number").textContent;
+	const input = document.getElementById("input").value;
+	const resultEl = document.getElementById("result");
+	resultEl.textContent = "Now loading result...";
+	
+	try{
+		const res = await fetch("https://latin-numerals-api.onrender.com/quiz_answer", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({question_number, input, streak}),
+	});
+		const data =  await res.json();
+		resultEl.textContent = data.result;
+		streak = data.streak;
+		sessionStorage.setItem("streak", streak);
+	} catch (e) {
+		resultEl.textContent = "Error: " + e.message;
+	}
+	
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("https://latin-numerals-api.onrender.com/quiz_query", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({}) 
+  })
+  .then(response => {
+	 
+    if (!response.ok) {
+		document.getElementById("question_number").textContent = "Network response was not ok";
+		throw new Error("Network response was not ok");
+		}
+    return response.json();
+  })
+  .then(data => {
+    console.log("Random quiz number:", data.number);
+    document.getElementById("question_number").textContent = data.number;
+  })
+  .catch(error => {
+    console.error("Fetch error:", error);
+  });
+});
